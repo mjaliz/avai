ce04ab57db8115c2f5ac2ac917ea5be1c70ce76b
mjaliz <mrph14@gmail.com>
2024-10-02 22:11:01 +0330

    fix spelling and grammatical issues using gpt

--- main.go
+++ main.go
@@ -24,4 +24,28 @@ func main() {
 	}
 	os.WriteFile("text.txt", []byte(resp.Text), 0622)
 	fmt.Println(resp.Text)
+	res, err := c.CreateChatCompletion(
+		ctx,
+		openai.ChatCompletionRequest{
+			Model: openai.GPT4oMini,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleSystem,
+					Content: "Fix spelling and grammatical issues of the provided persian content",
+				},
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: resp.Text,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+	fmt.Println(res.Choices[0].Message.Content)
+	os.WriteFile("text_fixed.txt", []byte(res.Choices[0].Message.Content), 0622)
+
 }
