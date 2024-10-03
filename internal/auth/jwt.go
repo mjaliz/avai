1494e321e392c3654869fc01aacbb2a15a0876a9
mjaliz <mrph14@gmail.com>
2024-10-03 16:29:56 +0330

    send sign key and expiration using config

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,14 +1,30 @@
 package auth
 
-import "github.com/golang-jwt/jwt"
+import (
+	"github.com/golang-jwt/jwt"
+	"strconv"
+	"time"
+)
 
 type Claim struct {
 	jwt.StandardClaims
 }
 
-func SignJWT(claim *Claim, key string) (string, error) {
+type SignConfig struct {
+	Key        string
+	ValidUntil time.Duration
+}
+
+func SignJWT(userId int64, conf SignConfig) (string, error) {
+	claim := Claim{
+		StandardClaims: jwt.StandardClaims{
+			Subject:   strconv.FormatInt(userId, 10),
+			IssuedAt:  time.Now().Unix(),
+			ExpiresAt: time.Now().Add(conf.ValidUntil).Unix(),
+		},
+	}
 	t := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
-	ss, err := t.SignedString([]byte(key))
+	ss, err := t.SignedString([]byte(conf.Key))
 	if err != nil {
 		return "", err
 	}
